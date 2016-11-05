4fa29a265f96d1c0930dd493e174ad911ae692b8
doooon <doooon>
2016-11-05 23:23:14 +0900

    b28

--- docs/portalGoGmaps.js
+++ docs/portalGoGmaps.js
@@ -15,7 +15,10 @@ http://maps.google.com/?daddr=31.595286,130.553541+(testtest)&ie=UTF8
   console.log("portalWindow: "+portalWindow);
   if (portalWindow==null) {
     var cookies = document.cookie.split(/;/);
-   console.log("cookies: "+cookies);   
+    for (var i in cookies) {
+      cookies[i]
+      console.log("cookies["+i+"]: "+cookies[i]);   
+    }
   }
 
 try {
