499a4127e78203e902b572c881bc294e6b420ad2
doooon <doooon>
2016-11-06 00:14:29 +0900

    b32

--- docs/portalGoGmaps.js
+++ docs/portalGoGmaps.js
@@ -1,10 +1,5 @@
 /* 
 javascript:(function(d){var s=d.createElement("script");s.src="https://doooon.github.io/Portal-go-googleMaps/portalGoGmaps.js";d.getElementsByTagName("head")[0].appendChild(s);})(document);
-
-http://maps.google.com/?q=%E8%A5%BF%E9%83%B7%E9%9A%86%E7%9B%9B%E5%83%8F@31.595286,130.553541
-http://maps.google.com/?q=31.595286,130.553541+(%E8%A5%BF%E9%83%B7%E9%9A%86%E7%9B%9B%E5%83%8F)
-http://maps.google.com/?q=%E8%A5%BF%E9%83%B7%E9%9A%86%E7%9B%9B%E5%83%8F@31.595286,130.553541&ie=UTF8
-http://maps.google.com/?daddr=31.595286,130.553541+(testtest)&ie=UTF8
 */
 
 
@@ -29,13 +24,9 @@ try {
     }
     latlng=lat+","+lng;
     console.log(latlng);
-
   } else {
-
     latlng=Hj.c.latLng.lat+","+Hj.c.latLng.lng; 
-    var portalName = document.getElementById("portal_primary_title").innerText; 
-  
-    // var str=encodeURIComponent(portalName);
+    // var portalName = encodeURIComponent(document.getElementById("portal_primary_title").innerText);
     console.log(latlng);    
   }
   
