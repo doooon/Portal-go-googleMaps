578c432381588a80eacdc6b5d900b587496cddcc
doooon <doooon>
2016-11-05 21:56:41 +0900

    b13

--- docs/portalGoGmaps.js
+++ docs/portalGoGmaps.js
@@ -1,24 +1,5 @@
 /* 
-javascript:
-  (
-    function(){ 
-      // ---------
-      
-      
-      // -------
-    } 
-  )(document);
-
 javascript:(function(d){var s=d.createElement("script");s.src="https://doooon.github.io/Portal-go-googleMaps/portalGoGmaps.js";d.getElementsByTagName("head")[0].appendChild(s);})(document);
-javascript:(function(d){var s=d.createElement("script");s.src="file:///Users/sakodon/Documents/%20gitHub/gitHub%20doooon/Portal-go-googleMaps/docs/portalGoGmaps.js";d.getElementsByTagName("head")[0].appendChild(s);})(document);
-
-
-################################### 
-ポータルが選択されていなかったら画面の中心をGmapの中心へ
-ポータルが選択されていたらGmapのドライブモードへ
-目的地 daddr=
-directionsmode:移動手段です。 driving、transit、bicycling、walking
-
 */
 
 
@@ -33,7 +14,8 @@ try {
   if (navigator.userAgent.match(/iPhone|iPad/i)) { 
     location.href="comgooglemaps://?q="+str; 
   } else { 
-    location.href="http://maps.google.com/?q="+str;
+    //location.href="http://maps.google.com/?q="+str;
+    window.open("http://maps.google.com/?q="+str,"_blank");
   } 
   
 } catch(e) { 
