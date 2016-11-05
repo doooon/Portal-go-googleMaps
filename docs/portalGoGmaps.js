5627aca077bb7fd9e46eaadc6b2718818ebe708f
doooon <doooon>
2016-11-05 19:22:31 +0900

    ファイル名変更

---
+++ docs/portalGoGmaps.js
@@ -0,0 +1,73 @@
+/* 
+javascript:
+  (
+    function(){ 
+      // ---------
+      
+      
+      // -------
+    } 
+  )(document);
+
+javascript:(function(d){var s=d.createElement("script");s.src="https://doooon.github.io/Portal-go-googleMaps/portalGoGmaps.js";d.getElementsByTagName("head")[0].appendChild(s);})(document);
+
+
+################################### 
+ポータルが選択されていなかったら画面の中心をGmapの中心へ
+ポータルが選択されていたらGmapのドライブモードへ
+目的地 daddr=
+directionsmode:移動手段です。 driving、transit、bicycling、walking
+
+*/
+
+
+
+function PortalDetail(){
+  return Hj
+}
+
+function ptdl2(){
+  return PortalDetail().c.latLng
+}
+
+function GetCookie(cookieName,separeter){ 
+  var cookieStr = document.cookie;
+  cookieStr = cookieStr.substring(
+                cookieStr.search(cookieName) + cookieName.length, 
+                cookieStr.indexOf(
+                  separeter, 
+                  cookieStr.search(cookieName)
+                )
+              )
+  return cookieStr; 
+}
+
+var latlng=""; 
+var ok=true; 
+var detail=false; 
+var portalName=""; 
+
+try {
+  if ( PortalDetail() == undefined || PortalDetail().getMap() ==null ){ 
+    detail=false; 
+  } else { 
+    detail=true; 
+    portalName = document.getElementById("portal_primary_title").innerText; 
+  } 
+  ok=true; 
+  
+} catch(e) { 
+  ok=false;
+  
+} finally { 
+  if(detail==false || ok == false){ 
+    latlng=GetCookie("ingress.intelmap.lat=",";")+","+GetCookie("ingress.intelmap.lng=",";"); 
+  }else{ 
+    latlng=ptdl2().lat+","+ptdl2().lng; 
+  } 
+  if (navigator.userAgent.match(/iPhone|iPad/i)) { 
+    location.href="comgooglemaps://?q="+latlng; 
+  } else { 
+    location.href="http://maps.google.com/?q="+latlng; 
+  } 
+} 
