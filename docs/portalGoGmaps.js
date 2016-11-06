77c06b791bc873cef9aeb744043f9219c788759a
doooon <doooon>
2016-11-06 15:55:44 +0900

    fix

--- docs/portalGoGmaps.js
+++ docs/portalGoGmaps.js
@@ -32,6 +32,7 @@ try {
     ) { 
       console.log("comgooglemaps://?center="+latlng);    
       alert("comgooglemaps://?center="+latlng);    
+      alert(navigator.userAgent);    
       location.href="comgooglemaps://?center="+latlng;
     } else { 
       console.log("http://maps.google.com/?ll="+latlng);    
