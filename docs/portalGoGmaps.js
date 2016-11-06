1ea52ec14c01b18b68192e78c4a256bdc249bc85
doooon <doooon>
2016-11-07 01:02:40 +0900

    cookieからの取得をやめる

--- docs/portalGoGmaps.js
+++ docs/portalGoGmaps.js
@@ -8,50 +8,39 @@ https://doooon.github.io/Portal-go-googleMaps/portalGoGmaps.js
 
 (function () {
   
-  //alert(document.getElementById("map_canvas")); 
-  //alert(document.getElementById("map_canvas").getElementsByTagName("a")); 
-  alert(document.getElementById("map_canvas").getElementsByTagName("a")[0]); 
-  //alert(document.getElementById("map_canvas").getElementsByTagName("a")[0].getAttributeNode("src")); 
-
   var latlng="";
   var portalWindow = document.getElementById("portal_info_windows");
   console.log("portalWindow: "+portalWindow);
+  // googleMapマークから現在地を取得
+  var url=document.getElementById("map_canvas").getElementsByTagName("a")[0];
+  // https://maps.google.com/maps?ll=31.598416,130.568321&z=19&t=m&hl=ja-JP&gl=US&mapclient=apiv3
+  var url_iOS=url.replace(/https:\/\/maps\.google\.com\/maps\?ll=/i, "comgooglemaps:\/\/?center=");
 
 try {
   
   if (portalWindow==null) {
     
     console.log("no portal_info_windows");
-    var lat="";
-    var lng="";
-    var cookies = document.cookie.split(/;/);
-    console.log("document.cookie: "+document.cookie);    
     
-    for (var i in cookies) {
-      
-      cookies[i]=cookies[i].split(/=/);
-      //console.log(cookies[i][0]+": "+cookies[i][1]);
-      if (cookies[i][0].match("ingress.intelmap.lat")) lat=cookies[i][1];
-      if (cookies[i][0].match("ingress.intelmap.lng")) lng=cookies[i][1];
-      
-    }
-    latlng=lat+","+lng;
-    console.log("latlng: "+latlng);
+    // googleMapマークから現在地を取得
+    var url=document.getElementById("map_canvas").getElementsByTagName("a")[0];
+    // https://maps.google.com/maps?ll=31.598416,130.568321&z=19&t=m&hl=ja-JP&gl=US&mapclient=apiv3
     
     if (navigator.userAgent.match(/iPhone|iPad/i)) { 
-      console.log("comgooglemaps://?center="+latlng);    
-      
-      location.href="comgooglemaps://?center="+latlng;
+      console.log("url_iOS);    
+      location.href=url_iOS;
     } else { 
-      console.log("http://maps.google.com/?ll="+latlng);    
-      
-      location.href="http://maps.google.com/?ll="+latlng;
-    } 
+      console.log("url);    
+      location.href=url;
+    }
 
   } else {
     
     console.log("get portal_info_windows");
     
+    var lat="";
+    var lng="";
+
     latlng=Hj.c.latLng.lat+","+Hj.c.latLng.lng;
     // var portalName = encodeURIComponent(document.getElementById("portal_primary_title").innerText);
     console.log("latlng: "+latlng);
